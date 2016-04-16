3aa9a0b15484a463990c32a3c5930a53981f7e05
alexey-ernest <alexey.ernest@gmail.com>
2016-04-16 14:32:09 +0600

    Refactoring + reply buttons

--- index.js
+++ index.js
@@ -12,6 +12,80 @@ var Cbr = require('./lib/cbr');
 var cbr = new Cbr();
 var city = require('./lib/city.js');
 
+/**
+ * ChatId:city cache.
+ *
+ * @type       {Object}
+ */
+var cityCache = {
+};
+
+/**
+ * Gets the best buy and sell rates together with cb rate.
+ *
+ * @method     getSellRate
+ * @param      {string}    cityCode  City code.
+ * @param      {Function}  fn        Callback function
+ */
+function getUsdRates(cityCode, fn) {
+  // requesting cbr USD rate and best exchange rates from banki.ru
+  async.parallel([
+    function(callback) {
+      banki.getUsdExchangeRate(cityCode, function (err, usd) {
+        if (err) return callback(err);
+        callback(null, usd);
+      });
+    },
+    function(callback) {
+      cbr.getUsdRate(function (err, rate) {
+        if (err) return callback(err);
+        callback(null, rate);
+      });
+    }
+  ], function(err, results) {
+    if (err) return fn(err);
+    fn(null, {cbr: results[1], exchange: results[0]});
+  });
+}
+
+/**
+ * Builds message with best sell rates in the city.
+ *
+ * @method     buildSellMessage
+ * @param      {string}    cityName  The name of the city to find best rates.
+ * @param      {Function}  fn        Callback function.
+ */
+function buildSellMessage(cityName, fn) {
+  var cityCode = city.getCode(cityName);
+
+  getUsdRates(cityCode, function (err, rates) {
+    if (err) return fn(err);
+    var city = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+    var msg = 'Курс ЦБ - ' + rates.cbr + 'р за 1 доллар. ' + 
+      'Дороже всего вы можете продать за ' + rates.exchange.buy.rate + 'р (' + city + ', ' + rates.exchange.buy.description + ')';
+    fn(null, msg);
+  });
+}
+
+/**
+ * Builds message with best buy rates in the city.
+ *
+ * @method     buildBuyMessage
+ * @param      {string}    cityName  The name of the city to find best rates.
+ * @param      {Function}  fn        Callback function.
+ */
+function buildBuyMessage(cityName, fn) {
+  var cityCode = city.getCode(cityName);
+
+  getUsdRates(cityCode, function (err, rates) {
+    if (err) return fn(err);
+    var city = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+    var msg = 'Курс ЦБ - ' + rates.cbr + 'р за 1 доллар. ' + 
+      'Дешевле всего вы можете купить доллар за ' + rates.exchange.sell.rate + 'р (' + city + ', ' + rates.exchange.sell.description + ')';
+    fn(null, msg);
+  });
+}
+
 var telegram = new Telegram(process.env.TELEGRAM_API_TOKEN)
 .on('update', function (update) {
   // parse message
@@ -35,48 +109,49 @@ var telegram = new Telegram(process.env.TELEGRAM_API_TOKEN)
     command = params.shift().toLowerCase();
   }
 
-  if (command === 'start') {
-    this.send('Введите город, чтобы узнать лучший курс. Например, Москва', update.message.from.id);
-  } else if (city.exists(command)) {
-    var cityCode = city.getCode(command);
-    var telegram = this;
-
-    // requesting cbr USD rate and best exchange rates from banki.ru
-    async.parallel([
-      function(callback) {
-        banki.getUsdExchangeRate(cityCode, function (err, usd) {
-          if (err) return callback(err);
-          callback(null, usd);
-        });
-      },
-      function(callback) {
-        cbr.getUsdRate(function (err, rate) {
-          if (err) return callback(err);
-          callback(null, rate);
-        });
-      }
-    ], function(err, results) {
-      if (err) return console.error(err);
-      
-      var usdExchange = results[0];
-      var usdCbr = results[1];
-      var city = command.charAt(0).toUpperCase() + command.slice(1);
-      var msg = 'Курс ЦБ - ' + usdCbr + 'р за 1 доллар. ' + 
-        'Дешевле всего вы можете купить доллар за ' + usdExchange.sell.rate + 'р (' + city + ', ' + usdExchange.sell.description + ')' + 
-        ', дороже всего вы можете продать за ' + usdExchange.buy.rate + 'р (' + city + ', ' + usdExchange.buy.description + ')';
-
-      if (isInline) {
-        telegram.sendArticleInline(msg, msg, chatId);
-      } else {
-        telegram.send(msg, chatId);
-      }
-    });
-  } else {
+  function sendMessage(err, msg) {
+    if (err) return console.error(err);
     if (isInline) {
-      this.sendArticleInline('Укажите город...', 'Укажите город...', chatId);
+      telegram.sendArticleInline(msg, msg, chatId);
     } else {
-      this.send('Укажите город...', chatId);
+      telegram.send(msg, chatId);
     }
+  }
+
+  var telegram = this;
+  if (command === 'start') {
+    this.send('Введите город, чтобы узнать лучший курс. Например, Москва', chatId);
+  } else if (cityCache[chatId] || city.exists(command)) {
+    var cityName, param;
+    if (city.exists(command)) {
+      cityName = cityCache[chatId] = command;
+      param = params.length > 0 ? params.shift().toLowerCase() : null;
+    } else if (cityCache[chatId]) {
+      cityName = cityCache[chatId];
+      param = command;
+    }
+
+    if (param === 'купить') {
+      buildBuyMessage(cityName, function (err, msg) {
+        sendMessage(err, msg);
+      });
+    } else if (param === 'продать') {
+      buildSellMessage(cityName, function (err, msg) {
+        sendMessage(err, msg);
+      });
+    } else {
+      // ask for action: sell/buy
+      var keyboard = {
+        keyboard: [[
+          {text: 'Купить USD'}, {text: 'Продать USD'}
+        ]],
+        one_time_keyboard: true
+      };
+
+      return this.send('Купить или продать?', chatId, keyboard);
+    }
+  } else {
+    sendMessage(null, 'Укажите город...');
   }
 })
 .listen();
